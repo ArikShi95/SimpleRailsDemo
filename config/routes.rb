353cd7b0ce71e6d7885415d2d2d2c512d96a9c3f
ArikShi95 <arikshi95@gmail.com>
2017-05-09 16:15:57 +0800

    server can use test

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"s
   root 'application#hello33'
-
+  test
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
